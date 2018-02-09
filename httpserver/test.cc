f5e4ebe3ea552a4bfd6cebef820e6e242e54d6e4
FeilongWu <wufeilongsky@outlook.com>
2018-02-09 17:39:36 +0800

    add test

--- httpserver/test.cc
+++ httpserver/test.cc
@@ -1,36 +1,57 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/epoll.h>
+#include <string>
+#include <iostream>
+#include <string.h>
+#include <cstdlib>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-class HttpServer {
-public:
-  HttpServer(const std::string& ip, const std::string& port, const std::string rootdir) : ip_(ip), port_(port), rootdir_(rootdir) {
-    struct sockaddr_in my_addr, peer_addr;
-    socklen_t peer_addr_size;
-    memset(&my_addr, 0, sizeof(my_addr));
-    my_addr.sun_family = AF_INET;
-    my_addr.sin_port = htons(port_.atoi());
-    inet_pton(AF_INET, ip_.c_str(), &my_addr.sin_addr);
-    fd_ = socket(AF_INET, SOCK_STREAM, 0);
-    auto ret = bind(fd_, (struct sockaddr*)&my_addr, sizeof(my_addr));
-    auto ret = listen(fd_, 1024);
+void setnonblocking(int sock)
+{
+  int opts;
+  if ((opts = fcntl(sock, F_GETFL)) < 0) {
+    printf("GETFL %d failed", sock);
+    exit(1);
   }
- 
-  int epoll() {
-    const MAX_EVENTS = 10
+  opts = opts | O_NONBLOCK;
+  if (fcntl(sock, F_SETFL, opts) < 0) {
+    printf("SETFL %d failed", sock);
+    exit(1);
+  }
+}
+
+void do_use_fd(int client_fd)
+{
+  const char str[] = "God bless you!\n";
+  if (send(client_fd,  str,  sizeof(str),  0) == -1)
+      perror("send");
+  close(client_fd);
+}
+
+
+int handle_date(std::string& ip, std::string& port) 
+{ 
+    const int MAX_EVENTS = 10;
     struct epoll_event ev, events[MAX_EVENTS];
     int listen_sock, conn_sock, nfds, epollfd;
+    struct sockaddr_in my_addr, local;
+    socklen_t addrlen;
+
+    memset(&my_addr, 0, sizeof(my_addr));
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(std::atoi(port.c_str()));
+    inet_aton(ip.c_str(), &my_addr.sin_addr);
+    listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    auto ret = bind(listen_sock, (struct sockaddr*)&my_addr, sizeof(my_addr));
+    ret = listen(listen_sock, 1024);
 
     epollfd = epoll_create1(0);
     if (epollfd == -1) {
       perror("epoll_create1");
-      exit(EXIT_FAILURE);
-    }
-
-    ev.events = EPOLLIN;
-    ev.data.fd = listen_sock;
-    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_sock, &ev) == -1) {
-      perror("epoll_ctl: listen_sock");
       exit(EXIT_FAILURE);
     }
 
@@ -48,7 +69,7 @@ public:
         exit(EXIT_FAILURE);
       }
 
-      for (n = 0; n < nfds; ++n) {
+      for (int n = 0; n < nfds; ++n) {
         if (events[n].data.fd == listen_sock) {
           conn_sock = accept(listen_sock,
                              (struct sockaddr *) &local, &addrlen);
@@ -72,15 +93,12 @@ public:
     }
   }   
   
-private:
-  File fd_;
 
-  std::string ip_("0.0.0.0");
-  std::string port_("80");
-  std::string rootdir_("/var/www");
-}
+  int main()
+  {
+    std::string ip("0.0.0.0");
+    std::string port("9999");
+    handle_date(ip, port);
 
-
-       int bind(int sockfd, const struct sockaddr *addr,
-                               socklen_t addrlen);
-
+    return 0;
+  }
