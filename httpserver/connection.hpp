97722db5623fa132e80bcd956ec0634b9d6db263
Trree <wufeilongsky@outlook.com>
2018-02-10 01:45:42 +0800

    add handle read

---
+++ httpserver/connection.hpp
@@ -0,0 +1,14 @@
+#ifndef HTTP_SREVER_CONNECTION_HPP_
+#define HTTP_SREVER_CONNECTION_HPP_
+
+namespace httpserver {
+
+class Connection {
+
+};
+
+
+} // namespace httpserver
+
+#endif // HTTP_SREVER_CONNECTION_HPP__
+
--- httpserver/server.hpp
+++ httpserver/server.hpp
@@ -38,6 +38,48 @@ public:
     }
   }
 
+  void handle_read(int client_fd) {
+    char buffer[1024];
+    std::size_t readlen = read(client_fd, buffer, sizeof(buffer));
+    printf("%s\n", buffer);
+
+    const char str[] = "God bless you!\n";
+    if (send(client_fd,  str,  sizeof(str),  0) == -1)
+      perror("send");
+    close(client_fd);
+  }
+
+  void handleEvent() {
+    struct sockaddr_in local;
+    socklen_t addrlen;
+    int  conn_sock;
+    for (;;) {
+      int nfds = epoll_wait(epollfd_, events_, 10, -1);
+      for (int n = 0; n < nfds; ++n) {
+        if (events_[n].data.fd == listen_fd_) {
+          printf("%d\n", listen_fd_);
+          conn_sock = accept(listen_fd_, (struct sockaddr *) &local, &addrlen);
+          if (conn_sock == -1) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+          }
+          setnonblocking(conn_sock);
+          struct epoll_event ev;
+          ev.events = EPOLLIN | EPOLLET;
+          ev.data.fd = conn_sock;
+          if (epoll_ctl(epollfd_, EPOLL_CTL_ADD, conn_sock,
+                        &ev) == -1) {
+                          perror("epoll_ctl: conn_sock");
+                          exit(EXIT_FAILURE);
+                        }
+        } else {
+          handle_read(events_[n].data.fd);
+        }
+      }
+    }
+  }
+
+private:
   void setnonblocking(int sock){
     int opts;
     if ((opts = fcntl(sock, F_GETFL)) < 0) {
@@ -51,45 +93,6 @@ public:
     }
   }
 
-  void do_use_fd(int client_fd) {
-    const char str[] = "God bless you!\n";
-    if (send(client_fd,  str,  sizeof(str),  0) == -1)
-    perror("send");
-    close(client_fd);
-  }
-
-  void handleEvent() {
-    struct sockaddr_in local;
-    socklen_t addrlen;
-    int  conn_sock;
-    for (;;) {
-      int nfds = epoll_wait(epollfd_, events_, 10, -1);
-      for (int n = 0; n < nfds; ++n) {
-        if (events_[n].data.fd == listen_fd_) {
-          conn_sock = accept(listen_fd_,
-                             (struct sockaddr *) &local, &addrlen);
-          if (conn_sock == -1) {
-            perror("accept");
-            exit(EXIT_FAILURE);
-
-          }
-          setnonblocking(conn_sock);
-          struct epoll_event ev;
-          ev.events = EPOLLIN | EPOLLET;
-          ev.data.fd = conn_sock;
-          if (epoll_ctl(epollfd_, EPOLL_CTL_ADD, conn_sock,
-                        &ev) == -1) {
-                          perror("epoll_ctl: conn_sock");
-                          exit(EXIT_FAILURE);
-                        }
-        } else {
-          do_use_fd(events_[n].data.fd);
-        }
-      }
-    }
-  }
-
-private:
   int listen_fd_;
   int epollfd_;
   struct epoll_event events_[10];
