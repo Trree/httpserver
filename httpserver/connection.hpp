af2d19441cdead5e4b4cea5ff2ecd5001f119550
FeilongWu <wufeilongsky@outlook.com>
2018-03-08 11:12:00 +0800

    package read

--- httpserver/connection.hpp
+++ httpserver/connection.hpp
@@ -25,39 +25,17 @@ public:
   }
   
   bool handleConnection() {
-    size_t len = MAXLEN;
-    char buffer[MAXLEN] = {0};
-    int size = Read(buffer, len);
-    if (-1 == size) {
-      std::cout << "errno: " << errno << ", " << strerror(errno) << '\n';
-      return false;
-    }
-    buffer_.assignBuffer(buffer, size);
-    if (checkComplete(buffer_.getBuffer())) {
+    if (handleRead()) {
       Request req(buffer_.getBuffer());
       std::string response = req.handleRequest();
-      handleWrite(response);
+      int wlen = handleWrite(response);
+      if (-1 == wlen) {
+        std::cout << "send errno: " << errno << ", " << strerror(errno) << '\n';
+        return false;
+      }
     }
     else {
       return false;
-    }
-    return true;
-  }
- 
-  bool checkComplete(const std::string header) {
-    if (!isComplete(header)) {
-      size_t expandsize = MMAXLEN;
-      char buffer[MMAXLEN] = {0};
-      int size = Read(buffer, expandsize);
-      if (-1 == size) {
-        std::cout << "errno: " << errno << ", " << strerror(errno) << '\n';
-        return false;
-      }
-      buffer_.assignBuffer(buffer, size);
-      if (!isComplete(buffer_.getBuffer())) {
-        throw std::runtime_error("http头读取不完整");
-        return false;
-      }
     }
     return true;
   }
@@ -66,20 +44,26 @@ public:
     size_t wlen = 0;
     int left = response.size();
     wlen = send(fd_, response.c_str(), left, 0);
+    std::cout << "send: fd:" << fd_ << ' ' << wlen << " of" << ' ' << left << '\n'; 
 
     return wlen;
   }
-
-private:
-
-  bool isComplete(std::string header) {
-    auto n = header.find("\r\n\r\n");
-    if (n == std::string::npos) {
+  
+  bool handleRead() {
+    size_t len = MAXLEN;
+    char buffer[MAXLEN] = {0};
+    int size = Read(buffer, len);
+    if (-1 == size) {
+      std::cout << "recv errno: " << errno << ", " << strerror(errno) << '\n';
+      return false;
+    }
+    buffer_.assignBuffer(buffer, size);
+    if (!checkComplete(buffer_.getBuffer())) {
       return false;
     }
     return true;
   }
- 
+
   int Read(char* buffer, size_t size) {
     ssize_t n = 0;
     do {
@@ -98,6 +82,33 @@ private:
     } while (errno != EINTR);
     
     return n;
+  }
+
+private:
+  bool isComplete(std::string header) {
+    auto n = header.find("\r\n\r\n");
+    if (n == std::string::npos) {
+      return false;
+    }
+    return true;
+  }
+
+  bool checkComplete(const std::string header) {
+    if (!isComplete(header)) {
+      size_t expandsize = MMAXLEN;
+      char buffer[MMAXLEN] = {0};
+      int size = Read(buffer, expandsize);
+      if (-1 == size) {
+        std::cout << "errno: " << errno << ", " << strerror(errno) << '\n';
+        return false;
+      }
+      buffer_.assignBuffer(buffer, size);
+      if (!isComplete(buffer_.getBuffer())) {
+        throw std::runtime_error("http头读取不完整");
+        return false;
+      }
+    }
+    return true;
   }
 
   int fd_{-1};
