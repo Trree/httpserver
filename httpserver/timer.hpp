2d6a135938536cd112826d98649b5d74831a262b
Trree <wufeilongsky@outlook.com>
2018-04-06 13:46:08 +0800

    add timer

---
+++ httpserver/timer.hpp
@@ -0,0 +1,13 @@
+#ifndef HTTP_SREVER_BUFFER_HPP_
+#define HTTP_SREVER_BUFFER_HPP_
+
+namespace httpserver {
+//这个定时器需要实现为一个非阻塞的吗？不然需要一个单独的线程去执行
+//它的wait需要如何去设计
+class Timer {
+
+
+
+}
+} // namespace httpserver
+#endif // HTTP_SREVER_BUFFER_HPP_
