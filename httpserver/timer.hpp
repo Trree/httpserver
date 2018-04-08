3c648851960f7cbd785a11e015de2f0b567eede4
Trree <wufeilongsky@outlook.com>
2018-04-08 22:13:52 +0800

    implement timer

--- httpserver/timer.hpp
+++ httpserver/timer.hpp
@@ -1,13 +1,46 @@
 #ifndef HTTP_SREVER_BUFFER_HPP_
 #define HTTP_SREVER_BUFFER_HPP_
 
+#include <chrono>
+#include <queue>
+#include <vector>
+#include <thread>
+#include <functional>
+#include <utility>
+
 namespace httpserver {
-//这个定时器需要实现为一个非阻塞的吗？不然需要一个单独的线程去执行
-//它的wait需要如何去设计
+
+using pair_t = std::pair<std::chrono::system_clock::time_point,std::function<void()>>;
+using container_t = std::vector<pair_t>;
+auto comp = [](const pair_t& e1, const pair_t& e2)
+            { return e1.first > e2.first; };
+
 class Timer {
+public:
+  Timer(const Timer&) = delete;
+  Timer& operator=(const Timer&) = delete;
 
+  explicit Timer(std::chrono::seconds sec, std::function<void()> fun) : time_(comp) {
+    time_.push(std::make_pair(sec + std::chrono::system_clock::now(), fun));
+  }
+  void insert(std::chrono::seconds sec, std::function<void()> fun) {
+    time_.push(std::make_pair(sec + std::chrono::system_clock::now(), fun));
+  }
+  void run() {
+    while(!time_.empty()) {
+      auto interval = std::chrono::system_clock::now() - time_.top().first;
+      if (interval.count() > 0) {
+        time_.top().second();
+        time_.pop();
+      }
+      else {
+        std::this_thread::sleep_for(interval);
+      }
+    }
+  }
 
-
-}
+private:
+  std::priority_queue<pair_t, container_t, decltype(comp)> time_;
+};
 } // namespace httpserver
 #endif // HTTP_SREVER_BUFFER_HPP_
