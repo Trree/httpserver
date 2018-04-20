49043c82ac997ba968059941ea0c8920f4747818
FeilongWu <wufeilongsky@outlook.com>
2018-04-20 13:59:51 +0800

    update

--- httpserver/http/handleMessage.hpp
+++ httpserver/http/handleMessage.hpp
@@ -1,14 +1,31 @@
 #ifndef HTTP_SREVER_HTTP_HANDLEMESSAGE_HPP_
 #define HTTP_SREVER_HTTP_HANDLEMESSAGE_HPP_
 
+#include "../connection.hpp"
+#include "response.hpp"
+#include "parse_uri.hpp"
 #include <memory>
 #include <string>
 
 namespace httpserver{
 
-class Connection;
-bool isComplete(std::string buffer);
-void handleMessage(std::shared_ptr<Connection> connptr, std::string& buffer);
+bool isComplete(std::string buffer) {
+  auto n = buffer.find("\r\n\r\n");
+  if (n == std::string::npos) {
+    return false;
+  }
+  return true;
+}
+
+void handleMessage(std::shared_ptr<Connection> connptr, std::string& buffer) {
+  ParseUri parseuri(buffer);
+  Response response(connptr);
+  if (parseuri.isKeepalive()) {
+    connptr->setKeepalive(true);
+  }
+  std::string rootdir("/var/www/html");
+  response.handleResponse(rootdir, parseuri.getRequestUri());
+} 
 
 } // namespace httpserver
 
